4116d31e49f3de5c2131728387cdd4e0563d7ce6
erc83 <espinoza.eric@gmail.com>
2021-05-06 22:59:49 -0400

    dependencias y Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import {Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import {Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
-import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import memories from './images/memories.png';
+import Navbar from './components/Navbar/Navbar';
+import { getPosts } from './actions/posts';
 import useStyles from './styles';
 
 const App = () => {
@@ -20,10 +20,7 @@ const App = () => {
 
     return ( 
         <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image}src={memories} alt="icon" height="60" />
-            </AppBar>
+            <Navbar />
             <Grow in>
                 <Container>
                     <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
